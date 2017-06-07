f6ea4e0ad45c683581d3268e82d2aed144a8df43
SamolazovRoman <samolazov.roman@gmail.com>
2017-06-07 15:33:08 +0300

    change for us-east-1

--- demo-8/instance.tf
+++ demo-8/instance.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  tags {
+    Name = "Terraform"
+  }
+
 
   # the VPC subnet
   subnet_id = "${aws_subnet.main-public-1.id}"
@@ -9,5 +13,5 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
 
   # the public SSH key
-  key_name = "${aws_key_pair.mykeypair.key_name}"
+  key_name = "${aws_key_pair.SamolazovRoman.key_name}"
 }
