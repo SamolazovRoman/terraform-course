9f25885de1dcf1ede033b6fe14e5be7a07ee1faf
SamolazovRoman <samolazov.roman@gmail.com>
2017-06-14 21:32:15 +0300

    change setting for work in ubuntu

--- packer-demo/amivar.tf
+++ packer-demo/amivar.tf
@@ -1,1 +1,1 @@
-variable "AMI_ID" { default = "ami-64786b02" }
+variable "AMI_ID" { default = "ami-cf4264d9" }
--- packer-demo/instance.tf
+++ packer-demo/instance.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "example" {
   ami           = "${var.AMI_ID}"
   instance_type = "t2.micro"
+  tags {
+    Name = "DevOps_box"
+  }
 
   # the VPC subnet
   subnet_id = "${aws_subnet.main-public-1.id}"
@@ -9,5 +12,5 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = ["${aws_security_group.example-instance.id}"]
 
   # the public SSH key
-  key_name = "${aws_key_pair.mykeypair.key_name}"
+  key_name = "${aws_key_pair.SamolazovRoman.key_name}"
 }
